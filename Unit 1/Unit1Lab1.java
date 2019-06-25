5b3469eca3af197f723d21adee8031f5c398dbca
Krish Thawani <krishthawani46@gmail.com>
2019-06-25 18:44:26 -0400

    Finished Unit1 Lab1

---
+++ Unit 1/Unit1Lab1.java
@@ -0,0 +1,20 @@
+/**
+ * This class/program produces an output of asterisks to display four symbols
+ * 
+ * @author Krish Thawani
+ *
+ */
+
+public class Unit1Lab1 {
+    public static void main(String[] args) {
+        System.out.println("*********\t***\t    *\t      *");
+        System.out.println("*\t*     *     *\t   ***\t     * *");
+        System.out.println("*\t*    *       *\t  *****\t    *   *");
+        System.out.println("*\t*    *       *\t    *\t   *     *");
+        System.out.println("*\t*    *       *\t    *\t  *       *");
+        System.out.println("*\t*    *       *\t    *\t   *     *");
+        System.out.println("*\t*    *       *\t    *\t    *   *");
+        System.out.println("*\t*     *     *\t    *\t     * *");
+        System.out.println("*\t*\t***\t    *\t      *");
+    }
+}
